6cde5bada061ed0b10ca3d3489cb316fc7677615
Harshil Gupta <harshil.gupta_cs19@gla.ac.in>
2021-05-06 10:06:00 +0530

    genrate last noun word

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Text(nouns.first)
+          child: Text(nouns.last)
         ),
       ),
     );
