b71eebd49e57c7b54de0ecfe39fd721793c6dbcf
Harshil Gupta <harshil.gupta_cs19@gla.ac.in>
2021-05-06 12:44:43 +0530

    add two flat button which play different sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
@@ -9,7 +10,37 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: )
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlatButton(
+                  onPressed: (){
+                    final players=AudioCache();
+                    players.play('note5.wav');
+                  },
+                  child: Text('Play 1',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20.0
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: (){
+                    final players=AudioCache();
+                    players.play('note7.wav');
+                  },
+                  child: Text('Play 2',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20.0
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ),
       ),
     );
