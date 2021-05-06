d5d8617e63060966fb3095aa733fb845281188cd
I_am_developer <harshilgupta0099@gmail.com>
2021-05-06 14:23:51 +0530

    add build key function

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,38 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  Expanded buildKey({Color color,int soundNumber}){
+    return Expanded(child: FlatButton(
+      color: color,
+      onPressed: (){
+        final players= AudioCache();
+        players.play('$soundNumber.wav');
+      },
+    ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Expanded(
-                  child: FlatButton(
-                    onPressed: (){
-                      final players=AudioCache();
-                      players.play('note1.wav');
-                      },
-                    color: Colors.red,
-                    ),
-                ),
-                FlatButton(
-                  onPressed: (){
-                    final players=AudioCache();
-                    players.play('note2.wav');
-                  },
-                  color: Colors.orange,
-                ),
-                FlatButton(
-                  onPressed: (){
-                    final players=AudioCache();
-                    players.play('note3.wav');
-                  },
-                  color: Colors.yellow,
-                ),
-                FlatButton(
-                  onPressed: (){
-                    final players=AudioCache();
-                    players.play('note4.wav');
-                  },
-                  color: Colors.green,
-                ),
-                FlatButton(
-                  onPressed: (){
-                    final players=AudioCache();
-                    players.play('note5.wav');
-                  },
-                  color: Colors.blueGrey,
-                ),
-                FlatButton(
-                  onPressed: (){
-                    final players=AudioCache();
-                    players.play('note6.wav');
-                  },
-                  color: Colors.blue,
-                ),
-                FlatButton(
-                  onPressed: (){
-                    final players=AudioCache();
-                    players.play('note7.wav');
-                  },
-                  color: Colors.purple,
-                ),
+                buildKey(color: Colors.red,soundNumber: 1),
+                buildKey(color: Colors.orange,soundNumber: 2),
+                buildKey(color: Colors.yellow,soundNumber: 3),
+                buildKey(color: Colors.green,soundNumber: 4),
+                buildKey(color: Colors.teal,soundNumber: 5),
+                buildKey(color: Colors.blue,soundNumber: 6),
+                buildKey(color: Colors.purple,soundNumber: 6),
               ],
             ),
           )
